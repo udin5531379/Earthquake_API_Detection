cc2fde2a568561e9dbbc13ae93fe3c40d70889c8
Udin Rajkarnikar <udinrajkarnikar@MacBook-Pro.local>
2020-08-30 10:05:52 +0545

    first Commit

---
+++ Earthquake_API_Detection/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Earthquake_API_Detection
+//
+//  Created by Udin Rajkarnikar on 20/8/30.
+//  Copyright © 2020 np.com.udinrajkarnikar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
